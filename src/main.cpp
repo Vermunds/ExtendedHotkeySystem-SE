1bfa48e831bbdf032e1a0e54545866cea43d2019
Vermunds <33235701+Vermunds@users.noreply.github.com>
2026-09-08 13:40:07 +0200

    Update logging setup

--- src/main.cpp
+++ src/main.cpp
@@ -30,12 +30,12 @@ extern "C"
 		log->flush_on(spdlog::level::trace);
 
 		spdlog::set_default_logger(std::move(log));
-		spdlog::set_pattern("%g(%#): [%^%l%$] %v", spdlog::pattern_time_type::local);
+		spdlog::set_pattern("%s(%#): [%^%l%$] %v", spdlog::pattern_time_type::local);
 
 		SKSE::log::info("{} v{} -({})", Version::FORMATTED_NAME, Version::STRING, __TIMESTAMP__);
 
 		SKSE::AllocTrampoline(1 << 5, true);
-		SKSE::Init(a_skse);
+		SKSE::Init(a_skse, false);
 
 		auto serialization = SKSE::GetSerializationInterface();
 		if (!serialization)
